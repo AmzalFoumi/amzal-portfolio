9a7cf5ad2ce5c703d5c3441d603d7fda75f96eae
AmzalFoumi <147682418+AmzalFoumi@users.noreply.github.com>
2026-07-19 23:54:50 +0530

    move projects up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
   return (
     <>
       <HeroSection />
-      <AboutSection />
       <ProjectsSection />
+      <AboutSection />
       <EducationSection />
       <ExperienceSection />
     </>
